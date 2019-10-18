3752bb03dcc526b2bfa8c67fa341a0d545e851de
W9t2333 <51908985+W9t2333@users.noreply.github.com>
2019-10-18 12:16:04 +0800

    Update socketserver.c

--- socketserver.c
+++ socketserver.c
@@ -5,7 +5,6 @@
 int main(int argc, char **argv) {
 	//加载套接字库，初始化
 	WSADATA	wd;
-	int connectNum = 5;
 	if (0 != WSAStartup(MAKEWORD(2, 2), &wd)) {
 		printf("加载套接字库失败，错误代码%d\n",GetLastError());
 		return 0;
@@ -19,7 +18,7 @@ int main(int argc, char **argv) {
 	//1.创建套接字 tcp传输控制协议 SOCK_STREAM   udp用户数据报协议 SOCK_DGRAM
 	SOCKET soktsv = socket(AF_INET, SOCK_STREAM, 0);
 	if (soktsv == INVALID_SOCKET) {
-		printf("创建套接字失败");
+		printf("创建套接字失败，错误代码%d\n", GetLastError());
 	}
 
 	//2.绑定socket和端口号
@@ -31,8 +30,8 @@ int main(int argc, char **argv) {
 		printf("绑定套接字失败，错误代码%d\n",GetLastError());
 	}
 
-	//3.监听listen 其中第二个参数backlog代表能够接收的最多的连接数
-	if (SOCKET_ERROR == listen(soktsv, connectNum)) {
+	//3.监听listen 其中第二个参数代表能够接收的最多的连接数
+	if (SOCKET_ERROR == listen(soktsv, 1)) {
 		printf("绑定套接字失败，错误代码%d\n", GetLastError());
 	}
 
@@ -42,28 +41,39 @@ int main(int argc, char **argv) {
 	while (1)
 	{
 		SOCKET soktaccpt = accept(soktsv, (SOCKADDR*)&accpt, &len);
-		char sendBuff[100]="";
-		char recvBuff[100];
-		printf("Client(%s:%d): connect success\n", inet_ntoa(accpt.sin_addr), service.sin_port);
+		char sendBuff[100] = "";
+		char recvBuff[100] = "";
+		printf("客户端(%s:%d)连接成功\n", inet_ntoa(accpt.sin_addr), service.sin_port);
 		sprintf(sendBuff, "您的ip为%s", inet_ntoa(accpt.sin_addr));
 		send(soktaccpt, sendBuff, strlen(sendBuff), 0);
 		while (1)
 		{
-			printf("请输入待发送的信息(按q退出)");
+			printf("请输入待发送的信息(输入q!退出):");
 			scanf("%s", &sendBuff);
-			if (strcmp(sendBuff, "q") == 0) {
-				printf("已经结束聊天");
-				sprintf(sendBuff, "%s已退出", inet_ntoa(accpt.sin_addr));
-				break;
+			if (strcmp(sendBuff, "q!") == 0) {
+				printf("您已经结束聊天");
+				send(soktaccpt, "q!",3, 0);
 				closesocket(soktaccpt);
+				break;
 			}
 			send(soktaccpt, sendBuff, sizeof(sendBuff), 0);
 			recv(soktaccpt, recvBuff, 100, 0);
+			if (strcmp(recvBuff, "q!") == 0) {
+				printf("%s已退出\n", inet_ntoa(accpt.sin_addr));
+				printf("输入q!退出:");
+				char Buff[20] = "";
+				scanf("%s", &Buff);
+				if (strcmp(Buff, "q!") == 0) {
+					break;
+				}
+			}
 			printf("%s：%s\n", inet_ntoa(accpt.sin_addr),recvBuff);
 		}
 		//5.关闭链接 close
 		SOCKET soktcls = closesocket(soktsv);
-		if (soktcls != SOCKET_ERROR) { break; }
+		if (soktcls != SOCKET_ERROR) { 
+			break; 
+		}
 	}
 	return 0;
 }
