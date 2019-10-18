e29fc66bf9f7f9e85e2000797c6aee1a094cdcde
W9t2333 <51908985+W9t2333@users.noreply.github.com>
2019-10-18 12:15:29 +0800

    Update socketclient.c

--- socketclient.c
+++ socketclient.c
@@ -1,3 +1,4 @@
+#pragma warning( disable : 4996)
 #include <stdio.h>
 #include <WinSock2.h>
 #pragma comment(lib,"ws2_32.lib")
@@ -18,7 +19,7 @@ int main(int argc, char **argv) {
 	//1.创建套接字socket tcp传输控制协议 SOCK_STREAM   udp用户数据报协议 SOCK_DGRAM
 	SOCKET soktclt = socket(AF_INET, SOCK_STREAM, 0);
 	if (FALSE == soktclt) {
-		printf("创建套接字失败");
+		printf("创建套接字失败，错误代码%d\n", GetLastError());
 	}
 	//2.连接 connect
 	SOCKADDR_IN Service;
@@ -29,26 +30,34 @@ int main(int argc, char **argv) {
 	if (soktcnct == SOCKET_ERROR) {
 		printf("连接服务器失败，错误代码%d\n", GetLastError());
 	}
-	char recvMsg[100] = { 0 };
-	char sendMsg[100];
+	char recvMsg[100] = "";
+	char sendMsg[100] = "";
 	recv(soktclt, recvMsg, 100, 0);
 	printf("服务器：%s\n", recvMsg);
 	while (1)
 	{
 		//接收消息
-		memset(recvMsg, 0, 100);
 		recv(soktclt, recvMsg, 100, 0);
+		if (strcmp(recvMsg, "q!") == 0) {
+			printf("%s已退出\n", inet_ntoa(Service.sin_addr));
+			printf("输入q!退出:");
+			char Buff[20] = "";
+			scanf("%s", &Buff);
+			if (strcmp(Buff, "q!") == 0) {
+				break;
+			}
+		}
 		printf("服务器：%s\n", recvMsg);
 		//发送消息
-		printf("请输入待发送的信息(按q退出)");
+		printf("请输入待发送的信息(按q!退出):");
 		scanf("%s", &sendMsg);
-		if (strcmp(sendMsg, "q") == 0) {
+		if (strcmp(sendMsg, "q!") == 0) {
 			printf("已经结束聊天");
+			send(soktclt, "q!", 3, 0);
 			break;
 			closesocket(soktclt);
 		}
 		send(soktclt, sendMsg, sizeof(sendMsg), 0);
 	}
-	getchar();
 	return 0;
 }
