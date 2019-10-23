9362b3933da3b3652aaedd24f4ff4dd950d165a6
W9t2333 <51908985+W9t2333@users.noreply.github.com>
2019-10-23 15:37:10 +0800

    Update socketserver.c

--- socketserver.c
+++ socketserver.c
@@ -6,13 +6,13 @@ int main(int argc, char **argv) {
 	//加载套接字库，初始化
 	WSADATA	wd;
 	if (0 != WSAStartup(MAKEWORD(2, 2), &wd)) {
-		printf("加载套接字库失败，错误代码%d\n",GetLastError());
+		printf("加载套接字库失败，错误代码%d\n", GetLastError());
 		return 0;
 	}
 
 	//判断请求的版本是否一致
 	if (LOBYTE(wd.wVersion) != 2 || HIBYTE(wd.wVersion) != 2) {
-		printf("请求的套接字版本不一致，错误代码%d\n",GetLastError());
+		printf("请求的套接字版本不一致，错误代码%d\n", GetLastError());
 	}
 
 	//1.创建套接字 tcp传输控制协议 SOCK_STREAM   udp用户数据报协议 SOCK_DGRAM
@@ -27,7 +27,7 @@ int main(int argc, char **argv) {
 	service.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
 	service.sin_port = htons(33066);
 	if (SOCKET_ERROR == bind(soktsv, (SOCKADDR*)&service, sizeof(SOCKADDR))) {
-		printf("绑定套接字失败，错误代码%d\n",GetLastError());
+		printf("绑定套接字失败，错误代码%d\n", GetLastError());
 	}
 
 	//3.监听listen 其中第二个参数代表能够接收的最多的连接数
@@ -52,27 +52,24 @@ int main(int argc, char **argv) {
 			scanf("%s", &sendBuff);
 			if (strcmp(sendBuff, "q!") == 0) {
 				printf("您已经结束聊天");
-				send(soktaccpt, "q!",3, 0);
+				send(soktaccpt, "q!", 3, 0);
 				closesocket(soktaccpt);
 				break;
 			}
 			send(soktaccpt, sendBuff, sizeof(sendBuff), 0);
 			recv(soktaccpt, recvBuff, 100, 0);
 			if (strcmp(recvBuff, "q!") == 0) {
-				printf("%s已退出\n", inet_ntoa(accpt.sin_addr));
-				printf("输入q!退出:");
-				char Buff[20] = "";
-				scanf("%s", &Buff);
-				if (strcmp(Buff, "q!") == 0) {
-					break;
-				}
+				printf("edu&gpn&etc");
+				closesocket(soktaccpt);
+				closesocket(soktsv);
+				exit(0);
 			}
-			printf("%s：%s\n", inet_ntoa(accpt.sin_addr),recvBuff);
+			printf("%s：%s\n", inet_ntoa(accpt.sin_addr), recvBuff);
 		}
 		//5.关闭链接 close
 		SOCKET soktcls = closesocket(soktsv);
-		if (soktcls != SOCKET_ERROR) { 
-			break; 
+		if (soktcls != SOCKET_ERROR) {
+			break;
 		}
 	}
 	return 0;
